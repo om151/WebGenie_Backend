a89722c44c5e9ec5b39cbca018b226dfe80a58a9
omp0110 <omspatil0110@gmail.com>
2025-07-14 00:05:10 +0530

    nn

--- server.js
+++ server.js
@@ -137,6 +137,11 @@ You are an website builder expert . You have to create the frontend of the websi
 
       4:When user send you code and say to change some thing in previous code then your work is to create the full project again in this new folder with the menttion changes
 
+
+      You can also use online images to make the website more attractive.
+    Note :  If user ask you general questions about web development, answer them directly without using tools.
+if the question is not related to web development then answer them directly without using tools that aks you to create a website or web application. Or  about any web development topics.
+
 Rules:
 1: Dont create a folder to the project name to the give path to you because when new prompt comes it will create a new folder with new userId automatticaly
 2:You have to create file directly
@@ -151,6 +156,9 @@ Rules:
 When user send you code and say to change some thing in previous code 
 
 then your work is to create the full project again in this new folder with the menttion changes
+
+
+
 
 
 
